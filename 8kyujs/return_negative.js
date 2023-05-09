a7d70ddea5f70d83896efe162cba5602a38705c1
Misha Awad <awadmishaa@gmail.com>
2023-05-09 16:01:32 +0900

    8kyu files added

---
+++ 8kyujs/return_negative.js
@@ -0,0 +1,8 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+// Examples
+
+// make_negative(1);  # return -1
+// make_negative(-5); # return -5
+// make_negative(0);  # return 0
+
+makeNegative = (num) => num > 0 ? -num : num
