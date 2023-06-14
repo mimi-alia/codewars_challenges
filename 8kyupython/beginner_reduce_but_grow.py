e3c62caacb26a2116e644523c7d5cb6e7e1064db
Misha Awad <awadmishaa@gmail.com>
2023-06-14 21:40:23 +0900

    8kyu challenges

---
+++ 8kyupython/beginner_reduce_but_grow.py
@@ -0,0 +1,14 @@
+# Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+# [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+def grow(arr):
+    num = 1
+        
+    for i in arr:
+        num*=i
+    return num
+        
+    
+
+    
